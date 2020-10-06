fbbf47d164bd6f648931ac2dfeed5082e5648f00
idontker <idontker@users.noreply.github.com>
2020-10-06 02:50:38 +0200

    created new pages and a lot of hacks: carfull it burnes and trust me on this. I can work

--- js/navbar.js
+++ js/navbar.js
@@ -1,26 +1,28 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
-    const test = document.querySelectorAll('nav div');
-    console.log( test);
-    burger.addEventListener('click', () => {
+    try{
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+        const test = document.querySelectorAll('nav div');
+        console.log( test);
+        burger.addEventListener('click', () => {
+            // Toggle Burger event
+            nav.classList.toggle('nav-active');
         
-        // Toggle Burger event
-        nav.classList.toggle('nav-active');
-        
-        //Animate Links
-        navLinks.forEach((link,index) => {
-            if(link.style.animation ){
-                link.style.animation = '' 
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.6}s`
-            }
+            //Animate Links
+            navLinks.forEach((link,index) => {
+                if(link.style.animation ){
+                    link.style.animation = '' 
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index /7 + 0.6}s`
+                }
+            });
+
+            // Burger Animation
+            burger.classList.toggle('toggle')
         });
-
-        // Burger Animation
-        burger.classList.toggle('toggle')
-    });
-
-   
+    }catch{
+        setTimeout(navSlide,100);
+        return;
+    }
 }
